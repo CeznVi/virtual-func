d2a2e153b425b52e167d33d44c94e5f040088677
Viktor <Cezn.Vi@gmail.com>
2022-08-24 20:58:14 +0300

    beta

--- Work_with_file.cpp
+++ Work_with_file.cpp
@@ -1,8 +1,21 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
+#include "file_load.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   SetConsoleCP(1251);
+   SetConsoleOutputCP(1251);
+
+   // std::cout << "Клас для роботи з файлами\n";
+
+   printFile pf;
+   pf.Display("text1.txt");
+    //lf.print();
+
+    
+
+    return 0;
 }
 
